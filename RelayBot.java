cfecce87bc6813fdf35547eb70a9ec594feae91b
Jason Freidman <jason@tokbox.com>
2010-09-30 20:13:22 -0700

    Cleaned up order of sections in README, change RelayBot to not print as much

--- RelayBot.java
+++ RelayBot.java
@@ -28,9 +28,9 @@ public final class RelayBot
 				d = Integer.parseInt(userInput.substring(2));
 			}
 			catch(java.lang.NumberFormatException e){
-				System.out.println("got bad value: " + userInput);
+				//System.out.println("got bad value: " + userInput);
 			}
-			System.out.println(userInput);
+			//System.out.println(userInput);
 
 			switch(userInput.charAt(0)) {
 			case 'q':
@@ -60,16 +60,16 @@ public final class RelayBot
 				case 1003:
 					if(key == 0)
 						key = InputEvent.BUTTON2_MASK;
-					System.out.print("got mouse event key = ");
-					System.out.println(key);
+					//System.out.print("got mouse event key = ");
+					//System.out.println(key);
 					switch(userInput.charAt(1)) {
 					case 'p':
 						robot.mousePress(key);
-					System.out.println("got mouse press");
+						//System.out.println("got mouse press");
 						break;
 					case 'r':
 						robot.mouseRelease(key);
-					System.out.println("got mouse release");
+						//System.out.println("got mouse release");
 						break;
 					}
 					break;
